ff39623c2813da67797a9aa61146ad998abd08d3
Hardeep Kaur <112050586+Hardeep1990@users.noreply.github.com>
2023-02-23 23:10:22 +1300

    new files

---
+++ level-5/mission1/src/components/Main.js
@@ -0,0 +1,11 @@
+import Robot from "./robot.js";
+import Customer from "./customer.js";
+function Main() {
+  return (
+    <div>
+      <Robot></Robot>
+      <Customer></Customer>
+    </div>
+  );
+}
+export default Main;
